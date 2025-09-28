15bc0e43181c95bf498735e0536b7f613d2c6b65
anlibrin <anlibrin@localhost.localdomain>
2025-09-29 00:52:14 +0100

    doing

---
+++ Studyplan/ReverseVowelsofaString.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdio.h>
+
+void    check_vowel(char s)
+{
+    if(s == 'a' || s == 'e' || s == 'i'  
+        || s == 'o' || s == 'u' || s == 'A' || s == 'E' || s == 'I'  
+        || s == 'O' || s == 'U' ||)
+
+}
+char* reverseVowels(char* s) {
+    
+}
+int main()
+{
+    char *s = "IceCream";
+    char *s = reverseVowels(s);
+    printf("%s\n",s);
+}
